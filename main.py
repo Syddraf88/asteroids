e1886f802a3e3a1f1edeb933ca36c4473791c5b6
Syddraf88 <acid47132@gmail.com>
2025-07-31 20:56:53 -0500

    Asteroids cosntants module

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
+def main():
+    print("Starting Asteroids!")
+    print(SCREEN_WIDTH)
+    print(SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+
+    main()
